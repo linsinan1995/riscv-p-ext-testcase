3e971e243cef7e87983c00bde1f48c87f8824055
Lin Sinan <sinan.lin@nokia.com>
2021-10-21 23:59:23 +0300

    fix wrong type

--- rvp64_scan/builtin-rvp64-smulx.c
+++ rvp64_scan/builtin-rvp64-smulx.c
@@ -1,6 +1,6 @@
 /* smtt32 also appears on filename, so scan-assembler-times plus 1 */
 /* { dg-do compile { target riscv64*-*-* } } */
-/* { dg-options "-march=rv64gc_zpn_zpsf -mabi=lp64d -O0" } */
+/* { dg-options "-march=rv64gc_zpn_zprv_zpsf -mabi=lp64d -O0" } */
 
 #include <rvp_intrinsic.h>
 #include <stdlib.h>
@@ -9,49 +9,49 @@
 static __attribute__ ((noinline))
 uint64_t ttms (uint32_t a, uint32_t b)
 {
-  return __rv_smul8 (ra, rb);
+  return __rv_smul8 (a, b);
 }
 
 static __attribute__ ((noinline))
-int16x4_t ttms_v (uint32_t a, uint32_t b)
+int16x4_t ttms_v (int8x4_t a, int8x4_t b)
 {
-  return __rv_v_smul8 (ra, rb);
+  return __rv_v_smul8 (a, b);
 }
 
 static __attribute__ ((noinline))
 uint64_t ttmsx (uint32_t a, uint32_t b)
 {
-  return __rv_smulx8 (ra, rb);
+  return __rv_smulx8 (a, b);
 }
 
 static __attribute__ ((noinline))
-int16x4_t ttmsx_v (uint32_t a, uint32_t b)
+int16x4_t ttmsx_v (int8x4_t a, int8x4_t b)
 {
-  return __rv_v_smulx8 (ra, rb);
-}
-
-static __attribute__ ((noinline))
-uint64_t ttms2 (uint32_t a, uint32_t b)
-{
-  return __rv_smul16 (ra, rb);
-}
-
-static __attribute__ ((noinline))
-int32x2_t ttms_v2 (uint32_t a, uint32_t b)
-{
-  return __rv_v_smul16 (ra, rb);
+  return __rv_v_smulx8 (a, b);
 }
 
 static __attribute__ ((noinline))
 uint64_t ttms2 (uint32_t a, uint32_t b)
 {
-  return __rv_smulx16 (ra, rb);
+  return __rv_smul16 (a, b);
 }
 
 static __attribute__ ((noinline))
-int32x2_t ttmsx_v2 (uint32_t a, uint32_t b)
+int32x2_t ttms_v2 (int16x2_t a, int16x2_t b)
 {
-  return __rv_v_smulx16 (ra, rb);
+  return __rv_v_smul16 (a, b);
+}
+
+static __attribute__ ((noinline))
+uint64_t ttms2 (uint32_t a, uint32_t b)
+{
+  return __rv_smulx16 (a, b);
+}
+
+static __attribute__ ((noinline))
+int32x2_t ttmsx_v2 (int16x2_t a, int16x2_t b)
+{
+  return __rv_v_smulx16 (a, b);
 }
 
 /* { dg-final { scan-assembler-times "smul8" 2 } } */
